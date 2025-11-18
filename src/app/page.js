3953209a5808b607fd540f27aaeff06b11ed47c7
Chintan64140 <chintan6414@gmail.com>
2025-11-18 19:08:49 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Landing from "../../Files/pages/Landing";
-
+export const metadata = {
+  title: "THE MONTA RO",
+  description: "Just wear it!",
+};
 export default function Home() {
   return <Landing />;
 }
